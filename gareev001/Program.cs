b638fe1fc2bb0d9bad0486de337806a499148fc4
imik838 <petrova.januly4@yandex.ru>
2020-02-26 11:59:29 +0500

    Init

---
+++ gareev001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gareev001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
